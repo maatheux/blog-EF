f44e62eddcd6828c6b30ce0c90fc6aa4b7ff9264
Matheus Lima <matheusmarinho.lima@hotmail.com>
2024-02-05 12:34:53 -0300

    CRUD

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -7,11 +7,11 @@ public class BlogDataContext : DbContext
 {
   public DbSet<Category> Categories { get; set; } // mapeando a entidade / recomendado usar o nome da prop como o do Model no plural
   public DbSet<Post> Posts { get; set; }
-  public DbSet<PostTag> PostTags { get; set; }
+  // public DbSet<PostTag> PostTags { get; set; }
   public DbSet<Role> Roles { get; set; }
   public DbSet<Tag> Tags { get; set; }
   public DbSet<User> Users { get; set; }
-  public DbSet<UserRole> userRoles { get; set; }
+  // public DbSet<UserRole> userRoles { get; set; }
 
   protected override void OnConfiguring(DbContextOptionsBuilder options)
     => options.UseSqlServer("Server=localhost,1433;Initial Catalog=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=true;");
--- Program.cs
+++ Program.cs
@@ -1,7 +1,49 @@
 ﻿
 using Blog.Data;
+using Blog.Models;
+
+Console.Clear();
 
 using (var contex = new BlogDataContext())
 {
-  
+  // CREATE
+  // Tag tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+  // contex.Tags.Add(tag);
+  // contex.SaveChanges(); // o contex salva em memoria, para persistir as alteracoes no banco utilizar o SaveChanges
+
+
+  // UPDATE
+  // Tag tag = contex.Tags.FirstOrDefault(x => x.Id == 5); // realizar o tracking, trazendo os dados do bd ao inves de criar usando New
+
+  // tag.Name = ".NET";
+  // tag.Slug = "dotnet";
+
+  // contex.Tags.Update(tag);
+  // contex.SaveChanges();
+
+
+  // DELETE
+  // Tag tag = contex.Tags.FirstOrDefault(x => x.Id == 5);
+
+  // contex.Tags.Remove(tag);
+  // contex.SaveChanges();
+
+
+  // SELECT
+  // IEnumerable<Tag> tags = contex.Tags; // nao executa no bd, eh uma referencia, mas tras os dados de Tags
+
+  IEnumerable<Tag> tags = contex.Tags.ToList();
+  // ATENTAR A ORDEM DOS FILTROS
+  // IEnumerable<Tag> tags = contex
+  //   .Tags
+  //   .ToList()
+  //   .Where(x => x.Name.Contains(".NET"));
+  // O where ira executar em memoria e nao no sql no bd, reduz a performace -> ToList como ultimo!
+
+
+  foreach (Tag tag in tags)
+  {
+    Console.WriteLine(tag.Name);
+  }
+
 }
