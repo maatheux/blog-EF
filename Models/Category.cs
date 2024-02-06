1bd645355f620361a3cd0db2380bbb9bd5689591
Matheus Lima <matheusmarinho.lima@hotmail.com>
2024-02-06 16:09:06 -0300

    Table, Key e Identity

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,13 @@
-﻿namespace Blog.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Models;
+
+[Table("Category")] // Mapeamento Data Annotations / caso nao informe o nome da tabela ira pegar o nome do DbSet
 public class Category
 {
+  [Key]
+  [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // gerado o id pelo banco
   public int Id { get; set; }
   public string Name { get; set; }
   public string Slug { get; set; }
--- Program.cs
+++ Program.cs
@@ -8,12 +8,4 @@ Console.Clear();
 using (var contex = new BlogDataContext())
 {
   
-  Tag? tag = contex
-    .Tags
-    .AsNoTracking()
-    .FirstOrDefault(x => x.Id == 3);
-    //.Single() => so pega se o item for unico
-
-  Console.WriteLine(tag?.Name);
-
 }
