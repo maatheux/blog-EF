c54dc5b3fbaf4fb708835a9360a6fec4815d6c29
Matheus Lima <matheusmarinho.lima@hotmail.com>
2024-02-06 17:29:12 -0300

    Required, MaxLenght e column

--- Models/Category.cs
+++ Models/Category.cs
@@ -9,7 +9,17 @@ public class Category
   [Key]
   [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // gerado o id pelo banco
   public int Id { get; set; }
+  
+  [Required]
+  [MinLength(3)]
+  [MaxLength(80)]
+  [Column("Name", TypeName = "VARCHAR")]
   public string Name { get; set; }
+
+  [Required]
+  [MinLength(3)]
+  [MaxLength(80)]
+  [Column("Slug", TypeName = "VARCHAR")]
   public string Slug { get; set; }
   
 }
