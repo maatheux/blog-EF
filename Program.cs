36c9a4362d9d00bbd487fa20e14977a1e8ba49dc
Matheus Lima <matheusmarinho.lima@hotmail.com>
2024-02-09 21:39:27 -0300

    Subconjuntos

--- Program.cs
+++ Program.cs
@@ -5,7 +5,35 @@ using Microsoft.EntityFrameworkCore;
 
 Console.Clear();
 
-using (var context = new BlogDataContext())
+using var context = new BlogDataContext();
+
+User user = new User
 {
-  
-}
+  Name = "André Baltieri",
+  Slug = "andrebaltieri",
+  Email = "andre@balta.io",
+  Bio = "11x Microsoft MVP",
+  Image = "https://balta.io",
+  PasswordHash = "12309876"
+};
+
+Category category = new Category
+{
+  Name = "Backend",
+  Slug = "backend"
+};
+
+Post post = new Post
+{
+  Author = user,
+  Category = category,
+  Body = "<p>Hello world</p>",
+  Slug = "comecando-com-ef-core",
+  Summary = "Neste artigo...",
+  Title = "Começando com EF Core",
+  CreateDate = DateTime.Now,
+  LastUpdateDate = DateTime.Now
+}; // O EF tem capacidade de referenciar os obj author e category, e conseguir inseri-los no banco
+
+context.Posts.Add(post);
+context.SaveChanges();
