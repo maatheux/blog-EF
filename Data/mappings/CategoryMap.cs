b49097b620aca6177b3e22fc5a6b63b4241c340b
Matheus Lima <matheusmarinho.lima@hotmail.com>
2024-02-12 23:17:25 -0300

    Mapeando propriedades

--- Data/mappings/CategoryMap.cs
+++ Data/mappings/CategoryMap.cs
@@ -8,15 +8,30 @@ public class CategoryMap : IEntityTypeConfiguration<Category>
 {
     public void Configure(EntityTypeBuilder<Category> builder)
     {
-        // Tabela
-        builder.ToTable("Category", "dbo");
+      // Tabela
+      builder.ToTable("Category", "dbo");
 
-        // Chave Primária
-        builder.HasKey(x => x.Id);
+      // Chave Primária
+      builder.HasKey(x => x.Id);
 
-        // Identity
-        builder.Property(x => x.Id)
-          .ValueGeneratedOnAdd()
-          .UseIdentityColumn(); // PRIMARY KEY IDENTITY (1, 1)
+      // Identity
+      builder.Property(x => x.Id)
+        .ValueGeneratedOnAdd()
+        .UseIdentityColumn(); // PRIMARY KEY IDENTITY (1, 1)
+      
+      // Propriedades
+      builder.Property(x => x.Name)
+        .IsRequired()
+        .HasColumnName("Name")
+        .HasColumnType("VARCHAR")
+        .HasMaxLength(80);
+    
+      builder.Property(x => x.Slug)
+        .IsRequired()
+        .HasColumnName("Slug")
+        .HasColumnType("VARCHAR")
+        .HasMaxLength(80);
+
+      
     }
 }
