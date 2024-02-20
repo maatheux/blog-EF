8fb1f881a2756f3f94a0afbba3e821474dad73cf
Matheus Lima <matheusmarinho.lima@hotmail.com>
2024-02-20 00:52:22 -0300

    skip and take

--- Program.cs
+++ Program.cs
@@ -6,23 +6,15 @@ using Microsoft.EntityFrameworkCore;
 Console.Clear();
 
 using var context = new BlogDataContext();
+var postList = GetPosts(context, 0, 25);
 
-// Lazy Loading
-var posts = context.Posts;
-foreach (var post in posts)
+
+static IList<Post> GetPosts(BlogDataContext context, int skip = 0, int take = 25)
 {
-  foreach (var tag in post.Tags)
-  {
-
-  }
+  var posts = context.Posts
+    .AsNoTracking()
+    .Skip(skip)
+    .Take(take)
+    .ToList();
+  return posts;
 }
-// Com a list de tags utilizando o mod de acesso 'virtual' permite utilizar o recurso de lazy loading, carregando apenas quando tags for chamada
-// Porem no EF nao eh mt interessante pois nao ira realizar o inner join e sim um select atras do outro
-
-
-// Eager Loading (default) -> so ira trazer os dados caso seja requerido a inclusao
-var postList = context.Posts.Include(x => x.Tags).Select(x => new
-{
-  Id = x.Id,
-  // ...
-}); // o select melhora na performace
